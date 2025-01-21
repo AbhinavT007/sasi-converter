f4d31a46bd19a7175888901d6c905bff535e0047
AbhinavT007 <anantha.tirumalakanduri@sasi.ac.in>
2025-01-21 16:18:09 +0530

    New change

--- convert.test.js
+++ convert.test.js
@@ -11,3 +11,6 @@ expect(convert(-100)).toBe('-1.20');
 test('handles non-numerical values', () => {
 expect(()=> convert('ten')).toThrow("Invalid amount");
 });
+test('handles non-numerical values', () => {
+expect(()=> convert('10n')).toThrow("Invalid amount");
+});
